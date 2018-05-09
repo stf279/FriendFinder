9cd0c4f8a1baf29c5a635ae03cd1486dde48abf2
stf279 <stf279@yahoo.com>
2018-05-08 20:28:37 -0500

    server.js completed

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require ("express");
+var bp = require ("body-parser");
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//Set the routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
+  
